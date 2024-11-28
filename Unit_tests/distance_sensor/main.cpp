72b499a8cf7a24f9bd156c868830de27b18c8df0
Bar Tzipori <bar.tzipori@gmail.com>
2024-11-28 02:38:42 +0200

    - Added a new unit test for the distance sensor. - Created the main.cpp file for the distance sensor unit test.

---
+++ Unit_tests/distance_sensor/main.cpp
@@ -0,0 +1,19 @@
+
+#include <Arduino.h>
+
+void setup() {
+    // Initialize serial communication at 115200 baud
+    Serial.begin(115200);
+
+    // Give some time for the Serial Monitor to initialize
+    delay(1000);
+
+    // Print a message to the Serial Monitor
+    Serial.println("Serial communication started!");
+}
+
+void loop() {
+    // Print a message repeatedly
+    Serial.println("Hello, this is a test message!");
+    delay(1000);  // Wait 1 second
+}
