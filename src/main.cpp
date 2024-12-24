facf8454837ac538928b049a8abc9715e392cb07
Bar Tzipori <bar.tzipori@gmail.com>
2024-12-24 16:55:14 +0200

    working version of initial integration

--- src/main.cpp
+++ src/main.cpp
@@ -14,23 +14,30 @@
 #define XSHUT_PIN_1 4
 #define XSHUT_PIN_2 5
 #define STACK_SIZE 2048
+#define MPU9250_ADDRESS 0x68
+#define VL53L1X_ADDRESS 0x60
+#define VL53L1X_ADDRESS_2 0x61
 
 Adafruit_VL53L1X vl53_1 = Adafruit_VL53L1X(XSHUT_PIN_1, IRQ_PIN);
 Adafruit_VL53L1X vl53_2 = Adafruit_VL53L1X(XSHUT_PIN_2, IRQ_PIN);
-std::vector<int> distance_sensors_xshut_pins = {XSHUT_PIN_1, XSHUT_PIN_2};
-std::vector<std::pair<Adafruit_VL53L1X*, int>> distance_sensors = {{&vl53_1, 0x60},  {&vl53_2, 0x61}};
 MPU9250 mpu;
+std::vector<int> distance_sensors_xshut_pins = {XSHUT_PIN_1, XSHUT_PIN_2};
+std::vector<std::pair<Adafruit_VL53L1X*, int>> distance_sensors = {{&vl53_1, VL53L1X_ADDRESS},  {&vl53_2, VL53L1X_ADDRESS_2}};
+std::vector<std::pair<MPU9250*, int>> mpu_sensors = {{&mpu, MPU9250_ADDRESS}};
 SensorData sensor_data;
-static int DistanceSensorDelay = 100;
+
+static int DistanceSensorDelay = 75;
 bool calibration_needed = false;
 bool system_calibrated = false;
+bool mpu_updated = false;
+
 TwoWire secondBus = TwoWire(1);
 
-//Samples VL53L1X sensors data
-void sampleVL53L1XSensorsData(void *pvParameters) {
+//Samples sensors data
+void sampleSensorsData(void *pvParameters) {
   int delay_in_ms = *(int *)pvParameters;
   while(true) {
-    vTaskDelay(delay_in_ms);
+    //samples distance sensors data
     for (int i = 0; i < distance_sensors.size(); i++) {
       if(!isVL53L1XSensorConnected(distance_sensors[i].second, &secondBus)) {
           Serial.print("Sensor: ");
@@ -43,7 +50,7 @@ void sampleVL53L1XSensorsData(void *pvParameters) {
               int distance = distance_sensors[i].first->distance();
               if (distance == -1) {
                 //Serial.print(F("Couldn't get distance: "));
-                Serial.println(distance_sensors[i].first->vl_status);
+                //Serial.println(distance_sensors[i].first->vl_status);
                 if(i == 0) {
                   sensor_data.setSensor1Distance(distance);
                 } else {
@@ -62,30 +69,22 @@ void sampleVL53L1XSensorsData(void *pvParameters) {
           }   
       }
     }
-    vTaskDelay(delay_in_ms);
-  }
-}
-//samples MPU sensor data
-void sampleMPUSensorData(void *pvParameters) {
-    int delay_in_ms = *(int *)pvParameters;
-    while (true) {
-        // Update MPU data
-        if (mpu.update()) {
-          sensor_data.setPitch(mpu.getPitch());
-          int yaw = mpu.getYaw();
-          if(yaw < 0 ) {
-            yaw = 360 + yaw;
-          }
-          sensor_data.setYaw(yaw);
-          sensor_data.setRoll(mpu.getRoll());
-          sensor_data.setAccelX(mpu.getLinearAccX());
-          sensor_data.setAccelY(mpu.getLinearAccY());
-          sensor_data.setAccelZ(mpu.getLinearAccZ());
-          sensor_data.setlastUpdateTime(millis());
-        }
-        // Wait for the next cycle
-        vTaskDelay(delay_in_ms);
+    //samples MPU data
+    if (mpu_sensors[0].first->update()) {
+      vTaskDelay(delay_in_ms);
+      sensor_data.setPitch(mpu_sensors[0].first->getPitch());
+      int yaw = mpu_sensors[0].first->getYaw();
+      if(yaw < 0 ) {
+        yaw = 360 + yaw;
+      }
+      sensor_data.setYaw(yaw);
+      sensor_data.setRoll(mpu_sensors[0].first->getRoll());
+      sensor_data.setAccelX(mpu_sensors[0].first->getLinearAccX());
+      sensor_data.setAccelY(mpu_sensors[0].first->getLinearAccY());
+      sensor_data.setAccelZ(mpu_sensors[0].first->getLinearAccZ());
+      sensor_data.setlastUpdateTime(millis());
     }
+  }
 }
 
 void setup() {
@@ -95,7 +94,7 @@ void setup() {
   Wire.setClock(100000); // Set I2C clock speed to 100 kHz
   delay(100);
   secondBus.begin(15,16);
-  secondBus.setClock(400000); // Set I2C clock speed to 100 kHz
+  secondBus.setClock(100000); // Set I2C clock speed to 100 kHz
 
   while (!Serial) delay(10);
 
@@ -108,30 +107,22 @@ void setup() {
   initializeVL53L1XSensor(distance_sensors[1].first, XSHUT_PIN_2, distance_sensors[1].second, &secondBus);  
 
   //Initializes MPU
-  if (!mpu.setup(0x68)) {  // change to your own address
+  if (!mpu.setup(MPU9250_ADDRESS)) {  // change to your own address
       while (1) {
           Serial.println("MPU connection failed. Please check your connection with `connection_check` example.");
           delay(5000);
       }
   }
   calibrateMPU(mpu, calibration_needed);
-  delay(15000);
+  delay(10000);
   system_calibrated = true;
-  
   //Creates threaded tasks
-  xTaskCreate(sampleVL53L1XSensorsData, "sampleVL53L1XSensorsData", STACK_SIZE, &DistanceSensorDelay, 3, nullptr);
-  xTaskCreate(sampleMPUSensorData, "sampleMPUSensorsData", STACK_SIZE, &DistanceSensorDelay, 2, nullptr);
+  xTaskCreate(sampleSensorsData, "sampleSensorsData", STACK_SIZE, &DistanceSensorDelay, 3, nullptr);
 }
 
 void loop() {
-  delay(100);
-  if(system_calibrated) {
-    static uint32_t prev_ms = millis();
-    if (millis() > prev_ms + 250)
-    {
-      //printMPURollPitchYaw(&mpu);
-      sensor_data.printData();
-      prev_ms = millis();
-    }
+  if (mpu.update() && system_calibrated) {
+        sensor_data.printData();
   }
+  delay(100);
 }
