43ddb4b74dc4c1af9a170345e00848dcb5501371
Bar Tzipori <bar.tzipori@gmail.com>
2025-01-17 17:03:22 +0200

    Refactor collision detection algorithm and update collision alert handling

--- include/collisionDetectionAlgorithm.h
+++ include/collisionDetectionAlgorithm.h
@@ -12,6 +12,10 @@
 void vibrateMotorsAsTask(void *pvParameters);
 void playMP3AsTask(void *pvParameters);
 void calculateStepCountAndSpeed(const SensorData& sensorData, int* stepCount, double* velocity, float userHeight);
-double collisionDetector(const SensorData& sensor_data, const systemSettings& system_settings, double* velocity);
+double nearestObstacleCollisionTime(const SensorData& sensor_data, const systemSettings& system_settings, double* velocity);
+double distanceToNearestObstacle(const SensorData& sensor_data, const systemSettings& system_settings, double* velocity);
+bool collisionTimeAlertHandler(double collision_time, systemSettings& system_settings, const MP3& mp3, vibrationMotor& motor1);
+bool obstacleDistanceAlertHandler(double obstacle_distance, systemSettings& system_settings, const MP3& mp3, vibrationMotor& motor1);
+double distanceToNearestObstacle(const SensorData& sensor_data, const systemSettings& system_settings, double* velocity);
 void collisionAlert(const systemSettings& system_settings, const MP3& mp3, vibrationMotor& vibration_motor, String vib_pattern, uint alert_sound_type);
 #endif
