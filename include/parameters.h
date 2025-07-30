c3ffb808f20dde54eb68b4dcd0801f4132019060
Bar Tzipori <bar.tzipori@gmail.com>
2025-07-30 17:09:54 +0300

    Update launch configuration and server URL
    - Changed the executable path and toolchain directory in .vscode/launch.json to reflect the new project location for debugging.
    - Updated the image recognition server URL in parameters.h to point to the correct IP address.

--- include/parameters.h
+++ include/parameters.h
@@ -169,7 +169,7 @@
 #define WEBMSG_SERVER_PORT 82 // Port for the webmsg server
 #define NTP_SERVER_1 "pool.ntp.org"
 #define NTP_SERVER_2 "time.nist.gov"
-#define IMGREC_serverUrl "http://172.20.10.2:5015/upload" // URL for image recognition server
+#define IMGREC_serverUrl "http://172.20.10.10:5015/upload" // URL for image recognition server
 
 
 //collision detection algorithm settings 
